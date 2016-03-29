67782c5a5b8eb03f106ce297bac32ab45378d264
niravshah <niravshah>
2016-03-29 15:45:22 +0000

    Test

---
+++ doupdate.sh
@@ -0,0 +1,4 @@
+!/bin/bash                                                                                                                                               
+# Git commit Bash Script                                                                                                                                  
+git pull origin master                                                                                                                                    
+pm2 restart app
