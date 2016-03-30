5e612b1425cb9e2fbe2a5f0acca597a1b7624fe2
niravshah <niravshah>
2016-03-30 08:03:45 +0000

    Updates

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,27 @@
-var BlogPost = require('./../models/blog');
-
+var BlogPost = require('./../models/posts');
 module.exports = function(app) {
     app.get('/posts', function(req, res) {
         BlogPost.find({}, function(err, users) {
             if(err) throw err;
             res.send(users);
+        });
+    });
+    
+    app.get('/posts/new', function(req, res) {
+        var newForm = BlogPost({
+            title: req.body.title,
+            author: req.body.author,
+            category: req.body.category,
+            description: req.body.description,
+            imageUrl:req.body.imageUrl,
+            comments:req.body.comments
+        });
+        newForm.save(function(err) {
+            if(err) throw err;
+            console.log('New Blog Post Saved');
+            res.json({
+                response: 'success'
+            });
         });
     });
 }
